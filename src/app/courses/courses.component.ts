7a227054b572bc1ea646fc6162e5fb3fd0637aa8
Jenny Lind <jeli2308@student.miun.se>
2024-05-22 21:13:12 +0200

    Lagt innehåll på undersidan Kurser och skrivit ut data i tabellen från json-filen. Gjort filtrerings- och sorteringsfunktioner. Ändrat färg och form för footer och header.

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,87 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CoursedataService } from '../services/coursedata.service';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  
+  // Egenskaper.
+  courseList: Course[] = [];
+  filteredCourseList: Course[] = [];
+  filterValue: string = "";
+  ascending: boolean = true;
+  subjects: string[] = []; // Array to hold unique subjects
 
+  // Konstruktor.
+  constructor(private coursedataService: CoursedataService) { }
+
+  // Initialisering.
+  ngOnInit() {
+    this.coursedataService.getCourses().subscribe(data => {
+      this.courseList = data;
+      this.filteredCourseList = data;
+      this.extractSubjects(data);
+    })
+  }
+
+  // Filtrering av kursnamn och/eller kurskod.
+  applyFilter(): void {
+    this.filteredCourseList = this.courseList.filter((course) =>
+      course.courseName.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.courseCode.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+
+  // Sortering av kurskod i växlande fallande/stigande ordning.
+  sortByCode(): void {
+    if (this.ascending) {
+      this.filteredCourseList.sort((a, b) => a.courseCode.localeCompare(b.courseCode));
+    } else {
+      this.filteredCourseList.sort((a, b) => b.courseCode.localeCompare(a.courseCode));
+    }
+    this.ascending = !this.ascending;
+  }
+
+  // Sortering av kursnamn i växlande fallande/stigande ordning.
+  sortByName(): void {
+    if(this.ascending) {
+      this.filteredCourseList.sort((a, b) => (a.courseName.localeCompare(b.courseName)));
+    } else {
+      this.filteredCourseList.sort((a, b) => (b.courseName.localeCompare(a.courseName)));
+    }
+    this.ascending = !this.ascending;
+  }
+
+  // Sortering av poäng i växlande fallande/stigande ordning.
+  sortByPoints(): void {
+    if(this.ascending) {
+      this.filteredCourseList.sort((a, b) => (a.points - b.points));
+    } else {
+      this.filteredCourseList.sort((a, b) => (b.points - a.points));
+    }
+    this.ascending = !this.ascending;
+  }
+
+  // Sortering av ämne i växlande fallande/stigande ordning.
+  sortBySubject(): void {
+    if(this.ascending) {
+      this.filteredCourseList.sort((a, b) => (a.subject.localeCompare(b.subject)));
+    } else {
+      this.filteredCourseList.sort((a, b) => (b.subject.localeCompare(a.subject)));
+    }
+    this.ascending = !this.ascending;
+  }
+
+  // Extrahera unika ämnen från json-filen.
+  extractSubjects(courses: any[]): void {
+    this.subjects = [...new Set(courses.map(course => course.subject))];
+  }
 }
--- src/app/services/coursedata.service.ts
+++ src/app/services/coursedata.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursedataService {
 
-  constructor() { }
+  // Privat egenskap.
+  file: string = "assets/miun_courses.json";
+
+  // Konstruktor.
+  constructor(private http: HttpClient) { }
+
+  // Funktion som hämtar en lista av kurser och returnerar en Observable.
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.file);
+  }
 }
